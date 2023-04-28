5e604ff77a3a01200876a91735fe573771b68d55
Andy P <apokk@tktk.ee>
2023-04-28 12:53:22 +0300

    add Week4 code

---
+++ Test/Program.cs
@@ -0,0 +1,17 @@
+﻿string userPIN;
+
+Console.WriteLine("Enter your PIN");
+userPIN = Console.ReadLine();
+while (true)
+    
+{
+    if (userPIN == "1234")
+    {
+        Console.WriteLine("Welcome!");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Wrong PIN. Try again.");
+    }
+}
